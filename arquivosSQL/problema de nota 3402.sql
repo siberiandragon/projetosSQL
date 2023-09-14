b5050c7b4712e2e35c6dc246efef016cea540941
felipeS <felipesantanabk@hotmail.com>
2023-09-14 08:34:08 -0300

    atualização diaria e inclusão do rascunho da contabilidade

--- arquivosSQL/problema de nota 3402.sql
+++ arquivosSQL/problema de nota 3402.sql
@@ -1,6 +1,14 @@
-select CODFILIAL from PCMOVCIAP where NUMTRANSENT ='15440';
+select * from PCMOVCIAP where NUMNOTA ='22051';
 
-select CODFILIAL,CODFILIALNF from PCNFENT where NUMTRANSENT ='15440';
+select CODFILIAL,CODFILIALNF from PCNFENT where NUMTRANSENT ='17705';
+
+select * from PCMOV where NUMNOTA ='22051';
+
+select * from PCPEDI where NUMPED ='1093001563';
+
+
+
+
 
 
 
