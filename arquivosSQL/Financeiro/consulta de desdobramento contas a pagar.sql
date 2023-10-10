1d7b5756ae70790f3b48ccd548d0eb9c38ff6744
Felipe's <134615110+siberiandragon@users.noreply.github.com>
2023-10-10 10:02:25 -0300

    Update consulta de desdobramento contas a pagar.sql
    update to public

--- arquivosSQL/Financeiro/consulta de desdobramento contas a pagar.sql
+++ arquivosSQL/Financeiro/consulta de desdobramento contas a pagar.sql
@@ -1,7 +1,7 @@
  select ( select DUPLIC 
           from PCPREST
           where 0=0
-          and NUMTRANSVENDA ='191305'
+          and NUMTRANSVENDA ='19130235'
           or DUPLIC = '53159'
           and PREST ='1'
           fetch first 1 rows only) as DUPLICATA,               
@@ -11,15 +11,15 @@
   from PCDESD D
 join PCPREST P on D.NUMTRANSVENDAORIG = P.NUMTRANSVENDA                          
 where 0=0  
-and NUMTRANSVENDADEST = '191305'  
+and NUMTRANSVENDADEST = '19130325'  
 and PRESTORIG = '1'     
-or P.DUPLIC ='53159'         
+or P.DUPLIC ='5315923'         
 union                                      
  select ( select DUPLIC 
           from PCPREST
           where 0=0
-          and NUMTRANSVENDA ='191305'
-          or DUPLIC = '53159'
+          and NUMTRANSVENDA ='19132305'
+          or DUPLIC = '5312359'
           and PREST ='1'
           fetch first 1 rows only) as DUPLICATA,              
        D.NUMTRANSVENDADEST,                                       
@@ -28,15 +28,15 @@ union
   from PCDESD D
 join PCPREST P on D.NUMTRANSVENDAORIG = P.NUMTRANSVENDA                           
 where 0=0
-and NUMTRANSVENDADEST = '191305'  
+and NUMTRANSVENDADEST = '19130235'  
 and PRESTDEST = '1'      
-or P.DUPLIC ='53159'            
+or P.DUPLIC ='5313259'            
 union                                      
  select ( select DUPLIC 
           from PCPREST
           where 0=0
-          and NUMTRANSVENDA ='191305'
-          or DUPLIC = '53159'
+          and NUMTRANSVENDA ='19132305'
+          or DUPLIC = '5323159'
           and PREST ='1'
           fetch first 1 rows only) as DUPLICATA,                
        M.NUMTRANSVENDADEST,                                          
@@ -45,16 +45,16 @@ union
   from PCDESDMAP M
 join PCPREST P on M.NUMTRANSVENDAORIG = P.NUMTRANSVENDA                         
 where 0=0
-and NUMTRANSVENDADEST = '191305'  
+and NUMTRANSVENDADEST = '19231305'  
 and PRESTORIG = '1'  
-or P.DUPLIC ='53159'
+or P.DUPLIC ='5312359'
                 
 union                                      
  select ( select DUPLIC 
           from PCPREST
           where 0=0
-          and NUMTRANSVENDA ='191305'
-          or DUPLIC = '53159'
+          and NUMTRANSVENDA ='12391305'
+          or DUPLIC = '5233159'
           and PREST ='1'
           fetch first 1 rows only) as DUPLICATA,                
        M.NUMTRANSVENDADEST,                                          
@@ -63,16 +63,16 @@ union
   from PCDESDMAP M
 join PCPREST P on M.NUMTRANSVENDAORIG = P.NUMTRANSVENDA                 
 where 0=0 
-and NUMTRANSVENDADEST = '191305'
-or P.DUPLIC ='53159'
+and NUMTRANSVENDADEST = '19132305'
+or P.DUPLIC ='5315239'
    
    ;
    
    
- select * from PCDESD where NUMTRANSVENDAORIG ='191305'
+ select * from PCDESD where NUMTRANSVENDAORIG ='19130235'
  ;
 
-select duplic from pcprest where numtransvenda ='191305' and PREST ='1';
+select duplic from pcprest where numtransvenda ='19123305' and PREST ='1';
 
 
 
